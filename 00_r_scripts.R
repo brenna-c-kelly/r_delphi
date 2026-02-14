556a3a178a5ad58827687498d1dcecf234316f68
Brenna Kelly <brennackelly@icloud.com>
2026-02-14 12:24:49 -0700

    renamed

---
+++ 00_r_scripts.R
@@ -0,0 +1,36 @@
+
+### an R script
+
+# here we'll learn to run code and perform basic operations
+
+# write a bit of code:
+print("hello world")
+# keep your cursor on the line, then evaluate by:
+# - clicking "run" in the GUI or
+# - cmd + enter (other shortcuts exist)
+
+# follow the same procedure with multiple lines by first highlighting:
+2 + 3
+print("hello world")
+
+# code can also be run directly in the console (the window below)
+# tip: using the arrow key allows you to quickly recover code you've executed (which makes editing much faster!)
+# tip: don't get attached to code you've executed in the console; if you want to keep it, put it in the document
+
+# assignment is the most basic command and follows the form:
+# <name> <assign> <data>
+# in R, assignment can be done using <- or =
+# try assigning data to a name
+x <- 2 + 3
+# you can display the object by entering the name or using `print()`
+x
+print(x)
+
+## try basic mathematical operations (*, +, /, -) with R
+# 1. choose a number and assign it to variable `x`
+# 2. multiple x by 3
+# 3. add 6
+# 4. divide by 3
+# 5. subtract x
+
+
